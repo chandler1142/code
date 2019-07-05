1aebca8dee447a02d39b742845729f2c9fe0b6c5
thzw1142 <thzw1142@163.com>
2019-07-05 17:14:01 +0800

    video crud

--- go_media/src/api/defs/apidefs.go
+++ go_media/src/api/defs/apidefs.go
@@ -4,3 +4,10 @@ type UserCredential struct {
 	Username string	`json:"username"`
 	Pwd string `json:"pwd"`
 }
+
+type VideoInfo struct {
+	Id string
+	AuthorId int
+	Name string
+	DisplayCtime string
+}
