d6a9e77d8a33988a27161c958affff1c1604517f
thzw1142 <thzw1142@163.com>
2019-07-06 09:39:50 +0800

    add comments

--- go_media/src/api/defs/apidefs.go
+++ go_media/src/api/defs/apidefs.go
@@ -1,13 +1,20 @@
 package defs
 
 type UserCredential struct {
-	Username string	`json:"username"`
-	Pwd string `json:"pwd"`
+	Username string `json:"username"`
+	Pwd      string `json:"pwd"`
 }
 
 type VideoInfo struct {
-	Id string
-	AuthorId int
-	Name string
+	Id           string
+	AuthorId     int
+	Name         string
 	DisplayCtime string
+}
+
+type Comment struct {
+	Id      string
+	VideoId string
+	Author  string
+	Content string
 }
