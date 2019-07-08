b31c76f817134bc6ca3f49054fc7ba1a6555efad
thzw1142 <thzw1142@163.com>
2019-07-08 21:41:58 +0800

    scheduler commit

--- go_media/src/scheduler/taskrunner/trmain.go
+++ go_media/src/scheduler/taskrunner/trmain.go
@@ -1,4 +1,30 @@
 package taskrunner
 
+import "time"
 
+type Worker struct {
+	ticker *time.Ticker
+	runner *Runner
+}
 
+func NewWorker(interval time.Duration, r *Runner) *Worker {
+	return &Worker{
+		ticker: time.NewTicker(interval * time.Second),
+		runner: r,
+	}
+}
+
+func (w *Worker) startWorker() {
+	for {
+		select {
+		case <-w.ticker.C:
+			go w.runner.startAll()
+		}
+	}
+}
+
+func Start() {
+	r := NewRunner(3, true, VideoClearDispatcher, VideoClearExecutor)
+	w := NewWorker(5, r)
+	go w.startWorker()
+}
